70056f137a790dd52e0f1593d73af2a4de3c6956
Elena <sice@oregonstate.edu>
2024-11-04 15:52:01 -0800

    Added randomness

--- character.py
+++ character.py
@@ -1,8 +1,10 @@
+import random
 class Character:
     def __init__(self, name, health, strength):
         self.name = name
         self.health = health
         self.strength = strength
+        self.is_alive = True
 
     def greet(self):
         print(f"{self.name}: Greetings!")
@@ -11,11 +13,18 @@ class Character:
         print(f"{self.name}: I am here to fight you {opponent_name}!")
     
     def attack(self, opponent):
+        damage = self.damage()
         print(f"{self.name} is attacking {opponent.name}!")
-        
-        print(f"{self.name} does {self.strength} damage!")
-        opponent.health = opponent.health - self.strength
-        print(f"{opponent.name} has {opponent.health} health remaining.")
+        print(f"{self.name} does {damage} damage!")
+        opponent.health = opponent.health - damage
+        if (opponent.health < 0):
+            opponent.is_alive = False
+            print(f"{opponent.name} has died!")
+        else:
+            print(f"{opponent.name} has {opponent.health} health remaining.")
+
+    def damage(self):
+        return self.strength + random.randrange(0,int((self.strength)/2)) - self.strength/4
         
 
 character_1 = Character("Bob", 50, 20)
@@ -26,5 +35,15 @@ character_2 = Character("Lilith", 100, 15)
 
 character_1.greet()
 character_2.instigate(character_1.name)
-print(character_2.health)
-character_1.attack(character_2)
+
+while character_1.health > 0 and character_2.health > 0:
+    if(random.randrange(0,2)) == 0:
+        character_1.attack(character_2)
+        if character_2.is_alive:
+            character_2.attack(character_1)
+    
+    else:
+        character_2.attack(character_1)
+        if character_1.is_alive:
+            character_1.attack(character_2)
+    
---
+++ randomness.py
@@ -0,0 +1,24 @@
+import time
+import random
+import math
+
+
+class LCG:
+   
+    def __init__(self, seed):
+        self.seed = seed
+        self.modulus = 2**31 - 1
+        self.multiplier = 48271
+        self.increment = 0
+
+    def random(self):
+        self.seed = self.seed * self.multiplier
+        self.seed = self.seed + self.increment
+        self.seed = self.seed % self.modulus
+        return self.seed / self.modulus
+    
+lcg = LCG(time.time())
+
+for i in range(10):
+    #print(math.floor(lcg.random()*11))
+    print(random.randint(0,1))
